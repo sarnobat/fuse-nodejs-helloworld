675b1d8ad55af0d6052e47505cfc25a581183e64
ssarnobat <ssarnobat@google.com>
2014-10-15 15:33:04 -0700

    Minor

--- run.sh
+++ run.sh
@@ -1,10 +1,1 @@
-
-sudo port install nodejs
-sudo port install npm
-
-mkdir hello_world
-cd hello_world
-npm install fuse4js
-ln -s node_modules/fuse4js
-mkdir /tmp/tutorial/mnt
-node fuse4js/example/jsonFS.js fuse4js/example/sample.json /tmp/tutorial/mnt
+hello_world_example/run.sh
